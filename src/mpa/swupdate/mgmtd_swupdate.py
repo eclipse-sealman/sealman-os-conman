347c93d81c32784fb4a8a6a2bd0dfc12cca37a63
Rafał Ilnicki <r.ilnicki@welotec.com>
2026-01-15 14:44:38 +0100

    Refactor code responsible for update (SWUpdate)
    - simplify communication over SWUpdate sockets
    - add more detailed error messages
    - add full_update_with_reboot() and use it mgmtd_swupdate.py and smartems.py
    - remove unnecessary parameters (dry_run, reboot) in update message
    - move SWUpdateDaemonMessage enum and consts to mgmtd_swupdate.py
    - change wrapped C enums to Python enums
    - remove unused code

--- src/mpa/swupdate/mgmtd_swupdate.py
+++ src/mpa/swupdate/mgmtd_swupdate.py
@@ -16,90 +16,131 @@
 import argparse
 import json
 import sys
+import time
+from enum import IntEnum
 from pathlib import Path
-from time import sleep
 from typing import Any, Callable, Dict, Optional
 
 # Local imports
 import mpa.communication.topics as topics
+import mpa.swupdate.swupdate as swupdate
 from mpa.common.common import RESPONSE_OK
 from mpa.common.logger import Logger
 from mpa.communication import client as com_client
 from mpa.communication.client import background, guarded
+from mpa.communication.message_parser import get_file
 from mpa.communication.common import SWUpdateError
-from mpa.communication.message_parser import get_bool, get_file
-from mpa.swupdate.swupdate_client import SWUpdateClient
-from mpa.swupdate.swupdate_types import RecoveryStatus, SwupdateDaemonMessages
+from mpa.device.common import reboot_device
 
 logger = Logger(f"{sys.argv[0] if __name__ == '__main__' else __name__}")
+_client: Optional[com_client.Client] = None
 
 
-def swupdate_progress_callback(swupdate_progess: dict[str, Any]) -> None:
-    __send_response_over_rt(swupdate_progess)
+class SwupdateDaemonMessage(IntEnum):
+    INFO = 0
+    SUCCESS = 1
+    ERROR = 2
 
 
-def __send_response_over_rt(message: dict[str, Any]) -> None:
-    _client.send(f"{topics.dev.swupdate}.rt", message)
+def __send_response_over_rt(data: dict[str, Any]) -> None:
+    assert _client is not None
+    _client.send(f"{topics.dev.swupdate}.rt", data)
+
+
+def full_update_with_reboot(
+    swupdate_file: Path,
+    *,
+    progress_callback: Optional[swupdate.ProgressCallback] = None,
+    description_filter: swupdate.DescriptionFilter = swupdate.default_descritpion_filter,
+    logging_func: Callable[[dict[str, Any]], None] = lambda _: None,
+) -> None:
+    """Perform a dry run, then update and finally reboot.
+
+    This function additionally sends real time messages to CLI and webui.
+    """
+    logging_func({
+        "type": "daemon_status",
+        "dry_run": True,
+        "data": {"message": "Running image check", "status": SwupdateDaemonMessage.INFO},
+    })
+    success, desc = swupdate.update(
+        dry_run=True,
+        swupdate_file=swupdate_file,
+        progress_callback=progress_callback,
+        description_filter=description_filter
+    )
+    if success:
+        logging_func({
+            "type": "daemon_status",
+            "dry_run": True,
+            "data": {"message": "Image check pass, running software update", "status": SwupdateDaemonMessage.INFO},
+        })
+    else:
+        logging_func({
+            "type": "daemon_status",
+            "dry_run": True,
+            "data": {"message": "Image check failed", "status": SwupdateDaemonMessage.ERROR},
+        })
+        raise SWUpdateError(desc)
+
+    # SWUpdate needs some time to finish processing an update
+    # it responds with NACK if new REQ_INSTALL comes too soon
+    time.sleep(5)
+
+    success, desc = swupdate.update(
+        dry_run=False,
+        swupdate_file=swupdate_file,
+        progress_callback=progress_callback,
+        description_filter=description_filter
+    )
+    if success:
+        logging_func({
+            "type": "daemon_status",
+            "dry_run": False,
+            "data": {
+                "message": "Update complete, system will reboot in about 10 seconds",
+                "status": SwupdateDaemonMessage.SUCCESS
+            },
+        })
+    else:
+        logging_func({
+            "type": "daemon_status",
+            "dry_run": False,
+            "data": {"message": "Software update failed!", "status": SwupdateDaemonMessage.ERROR},
+        })
+        raise SWUpdateError(desc)
+
+    reboot_device(b"")
 
 
 def perform_update(message: bytes) -> Optional[str]:
+    def progress_callback(nsteps: int, cur_percent: int, cur_step: int, dry_run: bool) -> None:
+        __send_response_over_rt(
+            {
+                "type": "progress",
+                "dry_run": dry_run,
+                "data": {"nsteps": nsteps, "cur_percent": cur_percent, "cur_step": cur_step},
+            }
+        )
+
     update_file_path = get_file(json.loads(message), "filepath")
-    dry_run = get_bool(json.loads(message), "dryrun")
-    reboot = get_bool(json.loads(message), "reboot")
-    logger.debug(f"SWUpdate reqest: {update_file_path=} {dry_run=} {reboot=}")
-    c = SWUpdateClient()
-
-    if dry_run:
-        __send_response_over_rt({"type": "daemon_status", "data":
-                                {"message": "Running image check",
-                                 "status": SwupdateDaemonMessages.INFO},
-                                "dry_run": dry_run})
-        check_status = c.perform_update(Path(update_file_path), dry_run, swupdate_progress_callback)
-        if check_status and check_status == RecoveryStatus.SUCCESS:
-            __send_response_over_rt({"type": "daemon_status", "data":
-                                    {"message": "Image check pass, running software update",
-                                     "status": SwupdateDaemonMessages.INFO},
-                                    "dry_run": dry_run})
-        else:
-            __send_response_over_rt({"type": "daemon_status", "data":
-                                    {"message": "Image check failed",
-                                     "status": SwupdateDaemonMessages.ERROR},
-                                    "dry_run": dry_run})
-            raise SWUpdateError("Image check fail")
-
-    # Perform full update
-    sleep(5)
-    update_status = c.perform_update(Path(update_file_path), False, swupdate_progress_callback)
-    logger.info(f"Update status: mgmtd-swupdate {str(update_status)}")
-    if update_status and update_status == RecoveryStatus.SUCCESS and reboot:
-        __send_response_over_rt({"type": "daemon_status", "data":
-                                {"message": "Update complete, system will reboot in about 10 seconds",
-                                 "status": SwupdateDaemonMessages.SUCCESS},
-                                 "dry_run": False})
-        sleep(5)
-        _client.query(topics.dev.reboot, "")
-    elif update_status == RecoveryStatus.SUCCESS:
-        __send_response_over_rt({"type": "daemon_status", "data":
-                                {"message": "Update complete!",
-                                 "status": SwupdateDaemonMessages.SUCCESS},
-                                 "dry_run": False})
-    elif update_status != RecoveryStatus.SUCCESS:
-        __send_response_over_rt({"type": "daemon_status", "data":
-                                {"message": "Software update failed!",
-                                 "status": SwupdateDaemonMessages.ERROR},
-                                 "dry_run": False})
-        raise SWUpdateError("Image install fail")
+    swupdate_file = Path(update_file_path)
+    full_update_with_reboot(
+        swupdate_file,
+        progress_callback=progress_callback,
+        logging_func=__send_response_over_rt
+    )
     return RESPONSE_OK
 
 
-_parser = argparse.ArgumentParser(prog='SWUpdate daemon')
-com_client.add_command_line_params(_parser)
-_args = _parser.parse_args()
-_client = com_client.Client(args=_args)
-messages: Dict[str, Any] = {}
-
-
 def main() -> None:
+    global _client
+    _parser = argparse.ArgumentParser(prog='SWUpdate daemon')
+    com_client.add_command_line_params(_parser)
+    _args = _parser.parse_args()
+    _client = com_client.Client(args=_args)
+    messages: Dict[str, Any] = {}
+
     def in_bg(topic: str, fun: com_client.SyncHandlerCallable, post_respond: Optional[Callable[[Any], None]] = None) -> None:
         messages[topic] = background(fun, com_client.respond_to(_client, topic), post_respond=post_respond)
     in_bg(topics.dev.swupdate, guarded(perform_update))
