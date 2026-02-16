2599560a08bcfb80fdd5f06ce07775c89d18b90e
Rafał Ilnicki <r.ilnicki@welotec.com>
2026-02-16 12:39:27 +0100

    Add Go mgmtd package

---
+++ mgmtd/mgmtd.go
@@ -0,0 +1,28 @@
+package mgmtd
+
+import (
+	"net"
+	"os"
+)
+
+const (
+	MgmtdUid = 16580
+	MgmtdGid = 16580
+)
+
+func CreateMgmtdUnixListener(socketPath string) (net.Listener, error) {
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := os.Chown(socketPath, MgmtdUid, MgmtdGid); err != nil {
+		return nil, err
+	}
+
+	if err := os.Chmod(socketPath, 0600); err != nil {
+		return nil, err
+	}
+
+	return listener, nil
+}
