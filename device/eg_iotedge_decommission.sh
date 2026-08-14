86ab9fae3d6358e84e559e8f4d76a813125e692a
Rafał Ilnicki <r.ilnicki@welotec.com>
2026-08-14 09:24:16 +0200

    Add missing copyright headers

--- device/eg_iotedge_decommission.sh
+++ device/eg_iotedge_decommission.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-
+#
+# Copyright (c) 2025 Contributors to the Eclipse Foundation.
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 # Used to decommission the device from IoT Hub
 # TODO
 # This is a temporary solution, this logic will be moved to specific daemon
